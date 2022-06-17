e23dd37965d753c58c34c799dfdfb367f2ec51ea
Omar Nava <omar.mac.97@gmail.com>
2022-06-17 12:02:23 -0500

    Initial Commit

---
+++ Zookeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zookeper
+//
+//  Created by Ricardo Omar Martinez Nava on 17/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zookeper/ZookeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeperApp.swift
+//  Zookeper
+//
+//  Created by Ricardo Omar Martinez Nava on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ZookeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
